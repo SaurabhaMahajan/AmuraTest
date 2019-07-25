00e503947efcc1689be637172ba44713d85060a7
saurabhmca29 <mahajansaurabh49@gmail.com>
2019-07-25 18:35:40 +0530

    Create Asssignment1.rb

---
+++ Asssignment1.rb
@@ -0,0 +1,13 @@
+class User
+  attr_accessor :status
+  ["active", "inactive", "pending"].each do |method_name|
+    define_method "#{method_name}?" do 
+      return self.status == method_name
+    end
+  end
+end
+
+@user = User.new
+@user.status = "inactive"
+@user.active?
+@user.inactive
